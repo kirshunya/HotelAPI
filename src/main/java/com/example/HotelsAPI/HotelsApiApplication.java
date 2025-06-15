4ba983b6f22693fa48d635384ac5ca674fe39359
kirshunya <bakunovich.nikita@mail.ru>
2025-06-15 15:23:54 +0300

    Initial commit from Spring Initializr

---
+++ src/main/java/com/example/HotelsAPI/HotelsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.HotelsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HotelsAPI/HotelsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HotelsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
